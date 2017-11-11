8d603e0067d502e9ee7c582aa27463bbc385be27
Ben Stutzman <33126601+BenRStutzman@users.noreply.github.com>
2017-11-11 15:34:47 -0500

    Added new functions
    Added functions for printing all instances of a word, and replacing a word with another.

--- Text_searcher.py
+++ Text_searcher.py
@@ -1,23 +1,38 @@
 import functions
 
 functions.welcome()
-
-action, text = functions.get_text()
+first_time = True
     
-while action != 'q':
-    action = functions.get_action()
-    if action == '0':
-        action, text = functions.get_text()
-        if action == 'q':
-            break
-    elif action == '1':
-        action, word = functions.get_word()
-        if action == 'q':
-            break
+while True:
+    try:
+        if first_time:
+            action = '0'
+            first_time = False
         else:
+            action = functions.get_action()
+        if action == '0':
+            text = functions.get_text()
+        elif action == '1':
+            word = functions.get_word("Please enter a word or phrase to "
+                                              "find in the text: ")
             functions.print_frequency(text, word)
-        
-    else:
+        elif action == '2':
+            word = functions.get_word("Please enter a word or phrase to "
+                                              "find in the text: ")
+            functions.print_instances(text, word)
+        elif action == '3':
+            old_word = functions.get_word("Please enter a word or phrase to "
+                                                  "find in the text: ")
+            new_word = functions.get_word("Please enter a word or phrase to "
+                                          "replace '%s': " % old_word.lower())
+            functions.replace_word(text, old_word, new_word)  
+        elif action == '4':
+            print("FIXME: Add encoding function.")
+        elif action == '5':
+            print("FIXME: Add special feature.")
+        else:
+            raise functions.QuitError
+    except functions.QuitError:
         break
 
-print('Have a nice day!')
+print('\nHave a nice day!')
--- functions.py
+++ functions.py
@@ -2,6 +2,9 @@
 
 import time
 
+class QuitError(Exception):
+    pass
+
 def welcome():
     """ Welcome the user and give instructions. """
     print("Hello there!")
@@ -20,15 +23,15 @@ def get_text():
     filename = input('Please enter the name of a text file, '
                      'including the extension: ')
     while True:
+        if filename == 'q':
+            raise QuitError
         try:
             with open(filename, 'r+') as f:
                 text = f.read()
-                print("\nYour text has been imported.")
-                return ('0', text)
+                print("Your text has been imported.")
+                time.sleep(1)
+                return text
         except FileNotFoundError:
-            if filename == 'q':
-                    return ('q', '')
-            else:
                 filename = input("Sorry, that file wasn't found. Enter "
                                 "another file name: ")
 
@@ -42,33 +45,78 @@ def get_action():
                             "(2) Find all instances of a word or phrase\n"
                             "(3) Replace a word or phrase with another word or phrase\n"
                             "(4) Encode the text with a Caesar cipher\n"
-                            "(5) Suprise me\n"
-                            "Enter the number corresponding to your choice: ")
+                            "(5) Surprise me\n"
+                            "\nEnter the number corresponding to your choice: ")
         if action in list('012345q'):
             return action
         else:
             print("Sorry, that's not a valid choice.")
 
-def get_word():
-    """ Ask the user for a word or phrase to search for. """
+def get_word(prompt):
+    """ Ask the user for a word with a given prompt, and return that word. """
     while True:
-        word = input("\nPlease enter a word or phrase to search for: ")
+        word = input(prompt)
         if word == 'q':
-            return ('q', '')
+            raise QuitError
         elif word:
-            return ('0', word.lower())
+            return word
         else:
             print("Sorry, make sure to enter a word or phrase.")
 
 def print_frequency(text, word):
-    frequency = text.count(word)
+    """ Print the number of times a string appears in a piece of text. """
+    frequency = text.lower().count(word.lower())
     if frequency == 1:
             plural = ''
     else:
         plural = 's'
-    print("'%s' appears %d time%s." %(word, frequency, plural))
+    if frequency == 0:
+        print("Sorry, '%s' doesn't appear in the text." % word.lower())
+    else:
+        print("'%s' appears %d time%s." %(word.lower(), frequency, plural))
+    time.sleep(1)
 
+def print_instances(text, word):
+    """ Print all instances of a word or phrase in the text,
+    plus some text around it. """
+    location = text.lower().find(word.lower())
+    if location == -1:
+        print("Sorry, '%s' doesn't appear in the text." % word.lower())
+        time.sleep(1)
+        return
+    else:
+        print("Here are all %d places '%s' appears:"
+              % (text.lower().count(word.lower()), word.lower()))
+    time.sleep(1)
+    old_location = -1
+    instance = 0
+    radius = 50
+    while location > old_location:
+        instance += 1
+        if radius <= location <= len(text) - radius:
+            print('\n%d) ...%s...' % (instance, text[location - radius:location + 50]))
+        elif location <= radius <= len(text) - radius:
+            print('\n%d) ...%s...' % (instance, text[:location + radius]))
+        elif radius <= len(text) - radius <= location:
+            print('\n%d) ...%s...' % (instance, text[location - radius:]))
+        else:
+            print('\n%d) ...%s...' % (instance, text))
+        old_location = location
+        location = text[location + 1:].lower().find(word.lower()) + location + 1
+    time.sleep(1)
 
+def replace_word(text, old_word, new_word):
+    """ Replace all instances of a word or phrase with another word or phrase,
+    then save the new text in a file. """
+    if old_word.lower() not in text.lower():
+        print("Sorry, '%s' doesn't appear in the text." % old_word.lower())
+        time.sleep(1)
+        return
+    with open('text_replaced.txt', 'w+') as f:
+        f.write(text.replace(old_word, old_word.lower())
+                .replace(old_word.lower(), new_word))
+    print("Your new text is in the file 'text_replaced.txt'.")
+    time.sleep(1)
 
 if __name__ == '__main__':
     print("This file contians the functions for 'Text_searcher.py'.")
